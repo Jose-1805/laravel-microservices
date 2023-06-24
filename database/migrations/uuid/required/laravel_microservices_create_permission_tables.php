002fdc282021d9cbbbb6050395771fa9743bdd17
jose-1805 <joseluiscapotemosquera@gmail.com>
2023-06-23 22:50:21 -0500

    Corrección en migración de tablas de permisos para evitar error en nombre de clase

--- database/migrations/uuid/required/laravel_microservices_create_permission_tables.php
+++ database/migrations/uuid/required/laravel_microservices_create_permission_tables.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Spatie\Permission\PermissionRegistrar;
 
-class CreatePermissionTables extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
@@ -145,4 +144,4 @@ class CreatePermissionTables extends Migration
         Schema::drop($tableNames['roles']);
         Schema::drop($tableNames['permissions']);
     }
-}
+};
